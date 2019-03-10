fe091741060e1642dc43cef98a712ec6bac66c2a
Ian Fabs <ianfabs@outlook.com>
2019-03-09 20:49:43 -0500

    Updated router

--- src/components/Router/Router.wc.js
+++ src/components/Router/Router.wc.js
@@ -4,7 +4,7 @@ class Router extends Component{
     constructor(routes){
         super();
         this.render = () => routes[window.location.pathname]
-        console.log(routes[window.location.pathname])
+        //console.log(routes[window.location.pathname])
     }
 }
 
